98f04e836d9bf01ff38f373fa9de5c079c7a9ae5
Guido Gauna <guidogauna9@gmail.com>
2026-04-15 00:32:38 -0300

    feat: upgrade version astro

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,32 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_B0Otaqcr.mjs';
+import { manifest } from './manifest_DLqIguv5.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/404.astro.mjs');
+const _page2 = () => import('./pages/api/chat.astro.mjs');
+const _page3 = () => import('./pages/api/contact.astro.mjs');
+const _page4 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/404.astro", _page1],
+    ["src/pages/api/chat.ts", _page2],
+    ["src/pages/api/contact.ts", _page3],
+    ["src/pages/index.astro", _page4]
+]);
+const serverIslandMap = new Map();
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "a7696bbf-80f3-40ae-a903-ce40fb763acd",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
